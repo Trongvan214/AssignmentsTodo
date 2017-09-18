30a9c446f48c031e2500318acc589e5596bbf20b
Trong Van <trongvan214@yahoo.com>
2017-09-18 10:56:09 -0500

    fix length null problem

--- main.js
+++ main.js
@@ -72,24 +72,27 @@ function showTodos() {
   var assigns = JSON.parse(localStorage.getItem('assigns'));
   var assignList = document.getElementById('assignList');
   assignList.innerHTML = '';
-  for(var i = assigns.length - 1; i >= 0; i--) {
-    var id = assigns[i].id;
-    var comment = assigns[i].comment;
-    var severity = assigns[i].severity;
-    var name = assigns[i].name;
-    var month = assigns[i].month;
-    var day = assigns[i].day;
-    var year = assigns[i].year;
-    var status = assigns[i].status;
-    assignList.innerHTML += '<div class="well">'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<p>'+remainingTime(year,month,day)+' Days left</p>'+
-                              '<h3 class="text-capitalize">' + name + '</h3>'+
-                              '<p class="pl-4">- ' + comment + '</p>'+
-                              '<p>Severity: ' + severity + '</p>'+
-                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
-                              '<a href="#" onclick="deleteAssign(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-                              '</div></br>';
+  if(!(assigns == null))
+  {
+    for(var i = assigns.length - 1; i >= 0; i--) {
+      var id = assigns[i].id;
+      var comment = assigns[i].comment;
+      var severity = assigns[i].severity;
+      var name = assigns[i].name;
+      var month = assigns[i].month;
+      var day = assigns[i].day;
+      var year = assigns[i].year;
+      var status = assigns[i].status;
+      assignList.innerHTML += '<div class="well">'+
+                                '<p><span class="label label-info">' + status + '</span></p>'+
+                                '<p>'+remainingTime(year,month,day)+' Days left</p>'+
+                                '<h3 class="text-capitalize">' + name + '</h3>'+
+                                '<p class="pl-4">- ' + comment + '</p>'+
+                                '<p>Severity: ' + severity + '</p>'+
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                                '<a href="#" onclick="deleteAssign(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                                '</div></br>';
+    }
   }
 }
 function remainingTime(year,month,day)
